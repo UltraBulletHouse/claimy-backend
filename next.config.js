d622d2feec03f0f2d5e62462aba6d5466ee67d4b
PiotrGrzechnik <ppgrzechnik@gmail.com>
2025-10-27 19:22:25 +0100

    Add async rewrites

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   output: 'standalone',
   experimental: {
     serverComponentsExternalPackages: ['firebase-admin', 'mongoose', 'graphql-yoga']
+  },
+  async rewrites() {
+    return [
+      { source: '/api/cases', destination: '/api/public/cases' },
+      { source: '/api/stores', destination: '/api/public/stores' },
+      { source: '/api/uploads', destination: '/api/public/uploads' },
+      { source: '/api/checkReplies', destination: '/api/public/checkReplies' }
+    ];
   }
 };
 
